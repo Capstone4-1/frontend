638568578b406376ed6c620223f8c35a2afdb040
LSYKR <zhzhzhgkgk@naver.com>
2025-03-25 00:17:11 +0900

    대시보드 레이아웃 나누기 완완

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,15 +5,21 @@ const Dashboard = () => {
     return (
         <div className="Dashboard">
             <div className="Content-Container">
-
-                
-                <div className="box"> 게시판판</div>
-                <div className="box"> 게시판판</div>
-                <div className="box"> 게시판판</div>
-                <div className="box"> 게시판판</div>
-                <div className="box"> 게시판판</div>
-                <div className="box"> 게시판판</div>
-                <div className="box"> 게시판판</div>
+                <div className="MainBoard">
+                    <div className="AnannouncementBoard">학과공지사항</div>
+                    <div className="TopDividedBoard">
+                        <div className="BulletinBoard">자유게시판</div>
+                        <div className="LectureBoard">강의게시판</div>
+                    </div>
+                    <div className="Market">장터</div>
+                    <div className="DownDividedBoard">
+                        <div className="HottopicBoard">인기게시판</div>
+                        <div className="SecretBulletinBoard">익명게시판</div>
+                    </div>
+                </div>
+                <div className="SideBoard">
+                    <div className="EmploymentinformationBoard">취창업정보</div>
+                </div>
             </div>
         </div>
     )
